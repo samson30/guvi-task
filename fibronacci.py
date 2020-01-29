02867f55ac21ad06107bc5cf2455aad014aa2658
samson30 <60385580+samson30@users.noreply.github.com>
2020-01-29 12:16:39 +0530

    Update fibronacci.py

--- fibronacci.py
+++ fibronacci.py
@@ -1,16 +1,16 @@
-def fibonacci(n): 
-    a = 0
-    b = 1
-    if n < 0: 
-        print("Incorrect input") 
-    elif n == 0: 
-        return a 
-    elif n == 1: 
-        return b 
-    else: 
-        for i in range(2,n): 
-            c = a + b 
-            a = b 
-            b = c 
-        return b 
-print(fibonacci(9)) 
+nterms = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+if nterms <= 0:
+       print("Please enter a positive integer")
+elif nterms == 1:
+       print("Fibonacci sequence upto",nterms,":")
+       print(n1)
+else:
+       print("Fibonacci sequence:")
+       while count < nterms:
+           print(n1)
+           nth = n1 + n2
+           n1 = n2
+           n2 = nth
+           count += 1
